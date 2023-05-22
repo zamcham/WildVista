ca16476e926c8253c9245802b86c4ca2c0c90998
zamcham <cristian@zamcham.com>
2023-05-22 11:55:28 -0400

    create components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import HomePage from './pages/HomePage';
+import DetailsPage from './pages/DetailsPage';
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <>
+      <HomePage />
+      <DetailsPage />
+    </>
+  );
 }
 export default App;
