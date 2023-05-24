54b778b6cbbe94d16f258d6095f60e1ad00d9f2d
zamcham <cristian@zamcham.com>
2023-05-24 12:30:50 -0400

    add search bar

--- src/components/StatesWithParks.js
+++ src/components/StatesWithParks.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import StateCard from './StateCard';
 
 const StatesWithParks = () => {
   const { statesData } = useSelector((store) => store.parkData);
-  // Convert the object's values into an array
-  const stateArray = Object.values(statesData);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter the stateArray based on the search term
+  const filteredStateArray = Object.values(statesData).filter((state) => {
+    const stateCode = state.stateCode.toLowerCase();
+    const search = searchTerm.toLowerCase();
+    return stateCode.includes(search);
+  });
 
   return (
-    <div className="states-container">
-      {stateArray.map((state) => (
-        <StateCard
-          key={state.id}
-          stateCode={state.stateCode}
-          stateParks={state.totalParks}
-          stateTotalActivities={state.totalActivities}
-          stateTotalTopics={state.totalTopics}
-        />
-      ))}
-    </div>
+    <>
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search state..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="states-container">
+        {filteredStateArray.map((state) => (
+          <StateCard
+            key={state.id}
+            stateCode={state.stateCode}
+            stateParks={state.totalParks}
+            stateTotalActivities={state.totalActivities}
+            stateTotalTopics={state.totalTopics}
+          />
+        ))}
+      </div>
+    </>
+
   );
 };
 
